9b80578e47f810474eb7823a2f70c8a6b20e7ff1
emmafrong1999-commits <emmafrong1999@gmail.com>
2025-10-15 13:48:27 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,29 +7,26 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-// ติดตั้ง service worker และ cache ไฟล์
+// ติดตั้ง Service Worker และ cache ไฟล์
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 // ดึงไฟล์จาก cache ก่อน แล้วค่อย fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 // อัปเดต cache และลบ cache เก่า
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => 
+    caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
